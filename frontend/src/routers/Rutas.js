01f48f150e94797232851d6ff66870a998e74730
Fabio Saez <the_fabio@live.cl>
2021-11-25 21:34:14 -0300

    Rutas-reparadas

--- frontend/src/routers/Rutas.js
+++ frontend/src/routers/Rutas.js
@@ -3,6 +3,11 @@ import { BrowserRouter, Route,  Routes } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import Login from '../pages/Login'
 import Register from '../pages/Register'
+import Populares from "../pages/Populares";
+import TodosLosAnalisis from "../pages/TodosLosAnalisis";
+import Mensajes from "../pages/Mensajes";
+import Rango from "../pages/Rango";
+import Configuracion from "../pages/Configuracion";
 
 const Rutas = () => {
     return (
@@ -11,7 +16,13 @@ const Rutas = () => {
                 <Routes>
                         <Route exact path="/register" element={<Register/>}/>   
                         <Route exact path="/login" element={<Login/>}/>
-                        <Route exact path="/" element={ <Navbar />} />   
+                        <Route exact path="/" element={ <Navbar />}> 
+                            <Route path="/populares" element={<Populares />} />
+                            <Route path="/analisis" element={<TodosLosAnalisis />} />
+                            <Route path="/mensajes" element={<Mensajes />} />
+                            <Route path="/rango" element={<Rango />} />
+                            <Route path="/configuracion" element={<Configuracion />} />
+                        </Route>
                 </Routes>
             </BrowserRouter>
         </>
